252de74582c4000d8f49151c1514a10ff614e914
Oscar Salvador <osalvador@suse.de>
2020-12-11 15:27:21 +0100

    powerpc/memhotplug: Enable MHP_MEMMAP_ON_MEMORY when supported
    Let the caller check whether it can pass MHP_MEMMAP_ON_MEMORY by
    checking mhp_supports_memmap_on_memory().
    MHP_MEMMAP_ON_MEMORY can only be set in case
    ARCH_MHP_MEMMAP_ON_MEMORY_ENABLE is enabled, the architecture supports
    altmap, and the range to be added spans a single memory block.
    
    Signed-off-by: Oscar Salvador <osalvador@suse.de>

--- arch/powerpc/platforms/pseries/hotplug-memory.c
+++ arch/powerpc/platforms/pseries/hotplug-memory.c
@@ -610,6 +610,7 @@ static int dlpar_memory_remove_by_ic(u32 lmbs_to_remove, u32 drc_index)
 
 static int dlpar_add_lmb(struct drmem_lmb *lmb)
 {
+	mhp_t mhp_flags = MHP_NONE;
 	unsigned long block_sz;
 	int nid, rc;
 
@@ -629,8 +630,10 @@ static int dlpar_add_lmb(struct drmem_lmb *lmb)
 	if (nid < 0 || !node_possible(nid))
 		nid = first_online_node;
 
+	if (mhp_supports_memmap_on_memory(block_sz))
+		mhp_flags |= MHP_MEMMAP_ON_MEMORY;
 	/* Add the memory */
-	rc = __add_memory(nid, lmb->base_addr, block_sz, MHP_NONE);
+	rc = __add_memory(nid, lmb->base_addr, block_sz, mhp_flags);
 	if (rc) {
 		invalidate_lmb_associativity_index(lmb);
 		return rc;
