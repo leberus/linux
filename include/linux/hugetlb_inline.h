c67c7e55d3fb50d88f22e9dd46d4e2f0d7f8ac59
Oscar Salvador <osalvador@suse.de>
2024-06-04 14:35:40 +0200

    mm,hugetlb: Add {p4d,pud,pmd}_vma_hugetlb helpers
    [BLOP]
    
    Signed-off-by: Oscar Salvador <osalvador@suse.de>

--- include/linux/hugetlb_inline.h
+++ include/linux/hugetlb_inline.h
@@ -11,9 +11,39 @@ static inline bool is_vm_hugetlb_page(struct vm_area_struct *vma)
 	return !!(vma->vm_flags & VM_HUGETLB);
 }
 
+static inline bool pmd_vma_hugetlb(pmd_t pmd, struct vm_area_struct *vma)
+{
+	return !!(pmd_leaf(pmd) && vma && is_vm_hugetlb_page(vma));
+}
+
+static inline bool pud_vma_hugetlb(pud_t pud, struct vm_area_struct *vma)
+{
+	return !!(pud_leaf(pud) && vma && is_vm_hugetlb_page(vma));
+}
+
+static inline bool p4d_vma_hugetlb(p4d_t pud, struct vm_area_struct *vma)
+{
+	return !!(p4d_leaf(p4d) && vma && is_vm_hugetlb_page(vma));
+}
+
 #else
 
 static inline bool is_vm_hugetlb_page(struct vm_area_struct *vma)
+{
+	return false;
+}
+
+static inline bool pmd_vma_hugetlb(pmd_t pmd, struct vm_area_struct *vma)
+{
+	return false;
+}
+
+static inline bool pud_vma_hugetlb(pud_t pud, struct vm_area_struct *vma)
+{
+	return false;
+}
+
+static inline bool p4d_vma_hugetlb(p4d_t p4d, struct vm_area_struct *vma)
 {
 	return false;
 }
