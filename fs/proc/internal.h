39a75beb2aee26a2b8becee3f5408ddfa28e1504
Oscar Salvador <osalvador@suse.de>
2025-02-14 22:21:06 +0100

    mm: Add /proc/pid/smaps_lab for debugging purposes
    Clone /proc/pid/smaps_lab off /proc/pid/smaps and make pt_range_walk API
    use the former, so we can check that the outcome is the same using both
    APIs (old one and pt_range_walk).
    
    [Only for debugging purposes]
    
    Signed-off-by: Oscar Salvador <osalvador@suse.de>

--- fs/proc/internal.h
+++ fs/proc/internal.h
@@ -404,6 +404,7 @@ struct mm_struct *proc_mem_open(struct inode *inode, unsigned int mode);
 extern const struct file_operations proc_pid_maps_operations;
 extern const struct file_operations proc_pid_numa_maps_operations;
 extern const struct file_operations proc_pid_smaps_operations;
+extern const struct file_operations proc_pid_smaps_operations_lab;
 extern const struct file_operations proc_pid_smaps_rollup_operations;
 extern const struct file_operations proc_clear_refs_operations;
 extern const struct file_operations proc_pagemap_operations;
